98814c3120a0c981846d66314be66c89dca8d990
Luana Faria Lane <luanasfff@gmail.com>
2019-11-19 21:49:41 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)  
- if /^[^aeiou\W]/i.match(s) == nil
-    return false
-  else
-    return true
-  end
+ if 
 end 
 
 def words_starting_with_un_and_ending_with_ing(text)
